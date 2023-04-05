322b2d5e9619365ccf61d1be349d0e4e1fe6390e
Ilya-Chayka99 <Ilya-Chayka99@yandex.ru>
2023-04-05 14:40:11 +0400

    1.0.0
    Добавлена шапка и  роутинг

---
+++ src/Components/appHeder/AppHeader.jsx
@@ -0,0 +1,28 @@
+import './AppHeader.css'
+import logo from '../../Components/appHeder/img/logo1.svg'
+import {Link,NavLink} from "react-router-dom";
+
+const AppHeader = () => {
+
+    return(
+        <header className="hed">
+            <img src={logo} alt="logo" className="logo"/>
+            <nav className="appMenu">
+                <ul>
+                    <li>
+                        <NavLink end style={({isActive})=>({color:isActive?'green':'white'})} to="/" className="nav">Фильмы</NavLink>
+                    </li>
+                    <li>
+                        <NavLink end style={({isActive})=>({color:isActive?'green':'white'})} to="/2" className="nav">Свои фильмы</NavLink>
+                    </li>
+
+                </ul>
+            </nav>
+        </header>
+
+    )
+
+
+}
+
+export default AppHeader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import 'reseter.css'
 import App from './App'
-import 'reseter.css'
 import './index.css'
 
 
